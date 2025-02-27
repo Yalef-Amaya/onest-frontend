a088ef28c6623f1420ab6acf8bacf230db5c3b4b
Yalef Amaya Morales <yalefamayamorales@MacBook-Air-de-Yalef.local>
2025-02-26 21:49:41 -0500

    [+] froms: Login y Users

--- src/app/pages/private/users/users.component.ts
+++ src/app/pages/private/users/users.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-users',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
 export class UsersComponent {
+  formData!: FormGroup;
 
+  constructor() {
+    this.formData = new FormGroup({
+      typeDoc: new FormControl('', [Validators.required]),
+      nDoc: new FormControl('', [ Validators.required, Validators.maxLength(10)]),
+      name: new FormControl('', [Validators.required] ),
+      username: new FormControl('', [ Validators.required, Validators.email ]),
+      celular: new FormControl('', [ Validators.required, Validators.maxLength(10), Validators.minLength(10) ]),
+      password: new FormControl('', [ Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      role: new FormControl('', [Validators.required])
+    });
+  }
+
+  onSubmit() {
+    const inputData = this.formData.value;
+
+    if( this.formData.valid ) {
+      console.log( inputData );
+    }
+
+    this.formData.reset();
+  }
 }
